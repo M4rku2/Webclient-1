72270ae80e9897d0e54118f06ca5f458e7ba242e
markus.schroettner <markus.schroettner@bulme.at>
2022-01-26 20:34:42 +0100

    Test Programm latest 26.01.2022

--- webclient.cpp
+++ webclient.cpp
@@ -1,7 +1,42 @@
 #include "webclient.h"
+#include <thread>
+
 
 Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+
+    connect(textinput, &QLineEdit::returnPressed, this, &Webclient::clicked_button);
+
+    m_sock = new QTcpSocket(this);
+    connect(m_sock, &QTcpSocket::connected, this, &Webclient::socket_connect);
+    connect(m_sock, &QTcpSocket::readyRead, this, &Webclient::print_text);
+}
+
+
+void Webclient::textoutput(QString message)
+{
+    output_text->clear();
+    output_text->setText(message);
+}
+
+
+void Webclient::socket_connect()
+{
+    m_sock->write(QString("GET / HTTP/1.1\r\nHost:" + m_search + "\r\n\r\n").toLocal8Bit());
+}
+
+
+void Webclient::print_text()
+{
+    textoutput(m_sock->readAll());
+    m_sock->disconnectFromHost();
+}
+
+
+void Webclient::clicked_button()
+{
+    m_sock->reset();
+    m_sock->connectToHost(textinput->text(), m_port);
 }
--- webclient.h
+++ webclient.h
@@ -2,13 +2,34 @@
 #define WEBCLIENT_H
 
 #include "ui_webclient.h"
+#include <QDebug>
+#include <QAbstractSocket>
+#include <QTcpSocket>
+
 
 class Webclient : public QWidget, private Ui::Webclient
 {
     Q_OBJECT
 
+
 public:
     explicit Webclient(QWidget *parent = nullptr);
+
+
+private slots:
+    void clicked_button();
+
+
+private:
+    void textoutput(QString message);
+    void socket_connect();
+    void print_text();
+
+
+private:
+    QTcpSocket *m_sock = nullptr;
+    QString m_search;
+    const short m_port = 80;
 };
 
 #endif // WEBCLIENT_H
